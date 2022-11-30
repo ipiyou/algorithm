6213d8b46402e72a1f18f00d8f6e47a4bf3730fc
Kim Tea Wan <82878304+ipiyou@users.noreply.github.com>
2022-11-30 17:16:19 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+long long dp[1001] = {};
+
+int main(){
+    int num;
+    cin >> num;
+    dp[1] = 1;
+    dp[2] = 3;
+    for(int i = 3;i <= num;i++){
+        dp[i] = (dp[i-1] + dp[i-2] + dp[i-2]) % 10007;
+    }
+    cout << dp[num];
+    return 0;
+}
